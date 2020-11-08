41cff93e041a1f1bd801406293371482100056ce
ghdpreto <ghdpreto@gmail.com>
2020-11-08 13:41:49 -0300

    Modificado a api para local

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
+const url = "http://192.168.1.7:3000"
+
 const api = axios.create({
-    baseURL: "http://www.campingfire.ga:8080/v1/"
+    baseURL: `${url}/v1/`
 })
 
 export default api
